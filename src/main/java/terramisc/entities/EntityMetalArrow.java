a4edcc4aa228627350a15763a62748369f952c9d
Svvarg <stizerblack@gmail.com>
2021-05-21 13:22:30 +0300

    EntityMetalArrow autoformat

--- src/main/java/terramisc/entities/EntityMetalArrow.java
+++ src/main/java/terramisc/entities/EntityMetalArrow.java
@@ -7,17 +7,15 @@ import terramisc.core.TFCMItems;
 
 import com.bioxx.tfc.Entities.EntityProjectileTFC;
 
-public class EntityMetalArrow extends EntityProjectileTFC 
-{
-	public Item pickupItem = TFCMItems.arrow_Copper;
-	
-	public EntityMetalArrow(World par1World) 
-	{
-		super(par1World);
-	}
+public class EntityMetalArrow extends EntityProjectileTFC {
 
-	public EntityMetalArrow(World par1World, EntityLivingBase shooter, float force)
-	{
-		super(par1World, shooter, force);
-	}
+    public Item pickupItem = TFCMItems.arrow_Copper;
+
+    public EntityMetalArrow(World par1World) {
+        super(par1World);
+    }
+
+    public EntityMetalArrow(World par1World, EntityLivingBase shooter, float force) {
+        super(par1World, shooter, force);
+    }
 }
