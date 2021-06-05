eff49010e4c4f874cd29ecc463a5571aed79a6ef
Svvarg <stizerblack@gmail.com>
2021-06-05 07:51:55 +0300

    ItemCustomHalberd autoformat

--- src/main/java/terramisc/items/tools/ItemCustomHalberd.java
+++ src/main/java/terramisc/items/tools/ItemCustomHalberd.java
@@ -15,63 +15,53 @@ import com.bioxx.tfc.api.Enums.EnumItemReach;
 import com.bioxx.tfc.api.Enums.EnumSize;
 import com.bioxx.tfc.api.Enums.EnumWeight;
 
-public class ItemCustomHalberd extends ItemWeapon
-{
-	public ItemCustomHalberd(ToolMaterial par2EnumToolMaterial, float damage, EnumDamageType dt)
-	{
-		super(par2EnumToolMaterial, damage);
-		this.damageType = dt;
-	}
+public class ItemCustomHalberd extends ItemWeapon {
 
-	public ItemCustomHalberd(ToolMaterial par2EnumToolMaterial, float damage)
-	{
-		super(par2EnumToolMaterial, damage);
-		this.damageType = EnumDamageType.SLASHING;
-		this.setCreativeTab(TFCTabs.TFC_WEAPONS);
-	}
-	
-	@Override
-	public void registerIcons(IIconRegister registerer)
-	{
-		this.itemIcon = registerer.registerIcon("tfcm:tools/"+this.getUnlocalizedName().replace("item.", ""));
-	}
+    public ItemCustomHalberd(ToolMaterial par2EnumToolMaterial, float damage, EnumDamageType dt) {
+        super(par2EnumToolMaterial, damage);
+        this.damageType = dt;
+    }
 
-	@Override
-	public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player)
-	{
-		return is;
-	}
-	
-	 @Override
-	 public void onUpdate (ItemStack stack, World world, Entity entity, int par4, boolean par5)
-	 {
-		 super.onUpdate(stack, world, entity, par4, par5);
-	     if (entity instanceof EntityPlayer)
-	     {
-	    	 EntityPlayer player = (EntityPlayer) entity;
-	         ItemStack equipped = player.getCurrentEquippedItem();
-	         if (equipped == stack)
-	         {
-	        	 player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, -1, 2, true));
-	         }
-	     }
-	 }
-	
-	@Override
-	public EnumItemReach getReach(ItemStack is)
-	{
-		return EnumItemReach.FAR;
-	}
-	
-	@Override
-	public EnumSize getSize(ItemStack is)
-	{
-		return EnumSize.LARGE;
-	}
+    public ItemCustomHalberd(ToolMaterial par2EnumToolMaterial, float damage) {
+        super(par2EnumToolMaterial, damage);
+        this.damageType = EnumDamageType.SLASHING;
+        this.setCreativeTab(TFCTabs.TFC_WEAPONS);
+    }
 
-	@Override
-	public EnumWeight getWeight(ItemStack is)
-	{
-		return EnumWeight.HEAVY;
-	}
+    @Override
+    public void registerIcons(IIconRegister registerer) {
+        this.itemIcon = registerer.registerIcon("tfcm:tools/" + this.getUnlocalizedName().replace("item.", ""));
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player) {
+        return is;
+    }
+
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5) {
+        super.onUpdate(stack, world, entity, par4, par5);
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            ItemStack equipped = player.getCurrentEquippedItem();
+            if (equipped == stack) {
+                player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, -1, 2, true));
+            }
+        }
+    }
+
+    @Override
+    public EnumItemReach getReach(ItemStack is) {
+        return EnumItemReach.FAR;
+    }
+
+    @Override
+    public EnumSize getSize(ItemStack is) {
+        return EnumSize.LARGE;
+    }
+
+    @Override
+    public EnumWeight getWeight(ItemStack is) {
+        return EnumWeight.HEAVY;
+    }
 }
